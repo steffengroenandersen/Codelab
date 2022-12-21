2b55d8a9d6b6c80b0397b7ba1c73ced57d8a3182
Steffen Andersen <steffengroenandersen@gmail.com>
2022-12-21 21:18:29 +0100

    Exercise 1

---
+++ 1. Semester/Level 1 - Basic/4 - [User input] - 10 exercises, some solutions/User1.java
@@ -0,0 +1,31 @@
+//Write a program to take two integer inputs from user
+//and print sum and product of them. 
+
+import java.util.*;
+
+
+public class User1{
+   public static void main(String[] args){
+      //get input from user
+      Scanner scanner = new Scanner(System.in);
+      int inputOne = 0;
+      int inputTwo = 0;
+      
+      //get inputOne and validate
+      try{
+         inputOne = scanner.nextInt();
+         inputTwo = scanner.nextInt();
+
+      }catch(InputMismatchException e){
+         System.out.println("Wrong format.");
+      }
+            
+      
+      //sum the input
+      int output = inputOne + inputTwo;
+      
+      //print the input
+      System.out.println(output);
+         
+   }
+}
