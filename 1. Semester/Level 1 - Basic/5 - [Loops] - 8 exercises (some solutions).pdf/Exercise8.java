da9c3b15bbdd296c0293cae214b1b0b155803781
Steffen Andersen <steffengroenandersen@gmail.com>
2022-12-22 13:19:50 +0100

    Create #8

---
+++ 1. Semester/Level 1 - Basic/5 - [Loops] - 8 exercises (some solutions).pdf/Exercise8.java
@@ -0,0 +1,12 @@
+//Write an infinite loop.
+//A inifinte loop never ends. Condition is always true.
+
+public class Exercise8{
+   public static void main(String[] args){
+      boolean amHappy = true;
+      
+      while(amHappy){
+         System.out.println("I'm a little program, and I am happy with my programmer :)");
+      }
+   }
+}
