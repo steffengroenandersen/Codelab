30bc953ad67200ee36d8a1bb2019f8cc01ea7b26
Steffen Andersen <steffengroenandersen@gmail.com>
2022-12-28 18:57:32 +0100

    Exericse 6+7

---
+++ 1. Semester/Level 1 - Basic/10 - [Arithmetics] - 10 assignments.pdf/Exercise7/Exercise7.java
@@ -0,0 +1,10 @@
+public class Exercise7{
+   public static void main(String[] args){
+      
+      double bitToByteRate = 8;
+      double byteToMegaByteRate = 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2 * 2;
+      
+        
+      System.out.println("1 Megabyte is " + (1 * bitToByteRate) * byteToMegaByteRate + " bits.");
+   }
+}
