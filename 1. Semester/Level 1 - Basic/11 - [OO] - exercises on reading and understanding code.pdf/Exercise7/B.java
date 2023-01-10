ba929981f9d34ae56acd7d4f5c219c6e3a756e74
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2023-01-10 20:50:28 +0100

    Added New Exercises.

--- 1. Semester/Level 1 - Basic/11 - [OO] - exercises on reading and understanding code.pdf/Exercise7/B.java
+++ 1. Semester/Level 1 - Basic/11 - [OO] - exercises on reading and understanding code.pdf/Exercise7/B.java
@@ -1,16 +1,15 @@
-public class B extends A{
-   int a = 50;
-   
-   public B(){
-      System.out.println("You've just called Class B's BASE Constructor.");
-      System.out.println("Variable a = " + a);
-      System.out.println();
-   }
-   
-   public B(int b){
-      System.out.println("You've just called Class B's ENHANCED Constructor.");
-      a = b;
-      System.out.println("Variable a = " + a);
-      System.out.println();
-   }
-}
+public class B extends A {
+ private int a = 123;
+ public B() {
+ a = 2222;
+ }
+ public void rollBackA () {
+ a = super.getA();
+ }
+ public void setA( int value) {
+ a = value;
+ }
+ public int getA() {
+ return a;
+ }
+} //class B
