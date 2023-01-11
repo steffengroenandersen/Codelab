47349769ddd814ed3f7422a4091c85dbaf599474
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2023-01-11 10:05:08 +0100

    Added Exercise2

---
+++ 1. Semester/Level 1 - Basic/12 - [Recursion, Arithmetics] - 5 exercises.pdf/Exercise2/Excercise2.java
@@ -0,0 +1,8 @@
+// Multiplication of non-negative integers can be defined recursively in terms of addition:
+// mult(n,0) = 0
+// mult(n,m+1) = n+mult(n+m)
+// Write a class which has a method mult which implements such a function.
+
+public class Excercise2{
+
+}
