8d12789ae1a0b4814fbbc870c66761844413659e
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2023-01-11 10:27:41 +0100

    Solved Exercise2

--- 1. Semester/Level 1 - Basic/12 - [Recursion, Arithmetics] - 5 exercises.pdf/Exercise2/Excercise2.java
+++ 1. Semester/Level 1 - Basic/12 - [Recursion, Arithmetics] - 5 exercises.pdf/Exercise2/Excercise2.java
@@ -4,5 +4,23 @@
 // Write a class which has a method mult which implements such a function.
 
 public class Excercise2{
+   public static void main(String[] args){
+      
+      int int1 = 2;
+      int int2 = 2;
+      
+      int answer = mult(int1, int2);
+      
+      System.out.println(answer);      
+   }
+   
+   // Multiplication method
+   public static int mult(int n, int m){
+      if(m == 0){
+         return 0;
+      }
+      
+      return n + mult(n,m-1);   
+   }
 
 }
