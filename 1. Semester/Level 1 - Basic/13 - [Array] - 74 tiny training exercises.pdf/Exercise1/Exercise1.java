b776abfc764430ce65d268fffd1d94211fec7a8f
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2023-01-11 12:54:19 +0100

    Exam Prep

---
+++ 1. Semester/Level 1 - Basic/13 - [Array] - 74 tiny training exercises.pdf/Exercise1/Exercise1.java
@@ -0,0 +1,5 @@
+// Write a Java program to sort a numeric array and a string array.
+
+public class Exercise1{
+   
+}
---
+++ Exam Prep/Eksamenstraening/untitled/src/Exercise1.java
@@ -0,0 +1,14 @@
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class Exercise1 {
+    public static void main(String[] args){
+        Integer a = Integer.valueOf(9);
+        a++;
+        System.out.println(a);
+
+        Boolean = true;
+        System.out.println():;
+    }
+}
