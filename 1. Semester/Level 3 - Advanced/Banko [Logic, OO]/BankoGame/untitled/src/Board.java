4025035e8d08fe5142d354b45a93141c516d4735
Steffen Grøn Andersen <steffengroenandersen@gmail.com>
2023-01-14 23:07:37 +0100

    Building Banko

---
+++ 1. Semester/Level 3 - Advanced/Banko [Logic, OO]/BankoGame/untitled/src/Board.java
@@ -0,0 +1,28 @@
+public class Board {
+
+    // Variables
+    int boardID;
+
+    boolean[] numberList = boolean[90];
+
+    int[] row1 = new int[9];
+    int[] row2 = new int[9];
+    int[] row3 = new int[9];
+
+    // Constructors
+
+    public Board(){
+
+    }
+
+    // Methods
+
+
+
+
+
+
+
+
+
+}
