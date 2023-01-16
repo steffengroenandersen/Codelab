1d8c110173946a2d7ca76654343ca725baafd423
Steffen Andersen <steffengroenandersen@gmail.com>
2023-01-16 21:43:35 +0100

    Pattern

--- Exam Prep/04 - Loops/Loops/src/StarPattern.java
+++ Exam Prep/04 - Loops/Loops/src/StarPattern.java
@@ -12,6 +12,7 @@ public class StarPattern {
             System.out.println();
         }
 
+
         // Print a triangle
         System.out.println();
         System.out.println("Print a triangle.");
@@ -57,6 +58,63 @@ public class StarPattern {
             System.out.println();
         }
 
+        // Print a rigt sided increasing triangle
+        System.out.println();
+        System.out.println("Print a Right Sided Increasing Triangle.");
+
+        int h = 5;
+
+        // Print 10 lines
+        for(int i = 1; i <= h; i++){
+            // Print decreasing triangle
+            for(int j = i; j <= n; j++){
+                System.out.print("  ");
+            }
+            // Print increasing star
+            for(int j = 1; j <= i; j++){
+                System.out.print("* ");
+            }
+            // Print increasing triangle
+
+            // Print new line
+            System.out.println();
+        }
+
+        // Print a Right Sided Triangle
+        System.out.println();
+        System.out.println("Print a Right Sided Decreasing Triangle.");
+
+        int u = 5;
+
+        for(int i = 1; i <= u; i++){
+
+            for(int j = 1; j < i; j++){
+                System.out.print("  ");
+            }
+
+            for(int j = i; j <= n; j++){
+                System.out.print(" *");
+            }
+
+
+            System.out.println();
+        }
+
+
+        // Printing hill pattern
+
+        System.out.println();
+        System.out.println("HILL PATTERN");
+        int o = 5;
+
+        for(int i = 1; i <= o; i++){
+
+            for(){
+
+            }
+
+            System.out.println();
+        }
 
 
     }
