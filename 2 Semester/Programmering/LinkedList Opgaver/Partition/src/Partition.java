f33048527360ffb00261d39d5ea522dbdd755205
Steffen Andersen <steffengroenandersen@gmail.com>
2023-02-07 22:22:17 +0100

    Finished Partition Task.

--- 2 Semester/Programmering/LinkedList Opgaver/Partition/src/Partition.java
+++ 2 Semester/Programmering/LinkedList Opgaver/Partition/src/Partition.java
@@ -1,14 +1,23 @@
 import java.util.Iterator;
 import java.util.LinkedList;
-import java.util.List;
+import java.lang.System;
 
 public class Partition {
-    public static void main(String[] args){
+    public static void main(String[] args) {
 
-        int elementValue;
+        int elementValue = 10;
+
+        System.out.println("Original List: ");
+        System.out.println(numbers);
+
+        partition(numbers, elementValue);
+
+        System.out.println("New List:");
+        System.out.println(numbers);
+
     }
 
-    static LinkedList<Integer> numbers = new LinkedList<>(){{
+    static LinkedList<Integer> numbers = new LinkedList<>() {{
         add(15);
         add(1);
         add(6);
@@ -23,17 +32,31 @@ public class Partition {
         add(9);
     }};
 
-    public static void partition(LinkedList<Integer> numbers), int elementValue{
+    public static void partition(LinkedList<Integer> numbers, int elementValue){
+
+        // Create iterator, LinkedList and temporary int
         Iterator<Integer> itr = numbers.iterator();
-        numbers.add
-        numbers
+        LinkedList<Integer> temporaryNumbers = new LinkedList<>();
+        int temporaryInt;
 
-
+        // Run through numbers and addFirst or addLast
         while(itr.hasNext()){
-            if(itr.next() < elementValue){
-                numbers.
+            temporaryInt = itr.next();
+            if(temporaryInt < elementValue){
+                //itr.remove();
+                temporaryNumbers.addFirst(temporaryInt);
+            } else{
+                //itr.remove();
+                temporaryNumbers.addLast(temporaryInt);
             }
+
         }
+
+
+        numbers.clear();
+        numbers.addAll(temporaryNumbers);
+
+
     }
 }
 
