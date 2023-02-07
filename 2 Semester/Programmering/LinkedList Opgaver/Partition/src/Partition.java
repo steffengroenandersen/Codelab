3ce39e064fae8f6623704306c2b50a035e37b4af
Steffen Andersen <steffengroenandersen@gmail.com>
2023-02-07 11:45:25 +0100

    Added Partion job

---
+++ 2 Semester/Programmering/LinkedList Opgaver/Partition/src/Partition.java
@@ -0,0 +1,39 @@
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Partition {
+    public static void main(String[] args){
+
+        int elementValue;
+    }
+
+    static LinkedList<Integer> numbers = new LinkedList<>(){{
+        add(15);
+        add(1);
+        add(6);
+        add(12);
+        add(-3);
+        add(4);
+        add(8);
+        add(21);
+        add(2);
+        add(30);
+        add(-1);
+        add(9);
+    }};
+
+    public static void partition(LinkedList<Integer> numbers), int elementValue{
+        Iterator<Integer> itr = numbers.iterator();
+        numbers.add
+        numbers
+
+
+        while(itr.hasNext()){
+            if(itr.next() < elementValue){
+                numbers.
+            }
+        }
+    }
+}
+
